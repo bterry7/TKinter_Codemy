a5c677c21ed91a675d00ba7599fd47fb886d8891
Benjamin Terry <bterry308@gmail.com>
2023-03-03 21:32:21 -0600

    Test

---
+++ TKinter-1.py
@@ -0,0 +1,5 @@
+import os # os is auto included with python. Other modules will not be
+os.system('clear') # use whatever command is needed for that instance; gitbash uses "clear"
+
+from tkinter import *
+
